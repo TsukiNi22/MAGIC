1169bf76ce949c441e7493243b0fa90f875cb199
Tsukini <mat.d.ecole@gmail.com>
2025-09-07 18:00:16 +0200

    feat (buttons tab): add the tab with the buttons at the top (changement of the window size)

--- src/const.py
+++ src/const.py
@@ -19,19 +19,55 @@ File Description:
 
 class Return:
     """
-    Return values, more details for the values in the original file
+    Return values
     """
     OK = 0 # Return value upon success on a call function
     KO = 1 # Return value upon fail on a call function
 
 class Error:
     """
-    Error values, more details for the values in the original file
+    Error values
     """
     FATAL_ERROR = 0b1000 # Global error, the program whole execution won't be able to run after this        (100% execution stop)
     LOCAL_ERROR = 0b100 # Local error, the program local execution won't probably be able to run after this (some chance of execution stop)
     ACTION_ERROR = 0b10 # Same~~ as Return.KO, a program action execution won't be able to run after this   (low chance of execution stop)
 
 class Window:
-    WIDTH = 1000 # X size of the window
-    HEIGHT = 600 # Y size of the window
+    """
+    Window size, color and other thing for the button, frame, window and other
+    """
+
+    # Main
+    WIDTH = 1200 # X size of the window
+    HEIGHT = 800 # Y size of the window
+
+    # Frame
+    FRAME_COLOR = "#2D2D2D" # Background color of the frames
+    FRAME_PADX = 15 # Padding X of the frames
+    FRAME_PADY = 15 # Padding Y of the frames
+
+    # Button
+    BUTTON_COLOR = "#2F64B4" # Background color of the buttons
+    BUTTON_FONT = ("Arial", 25) # Font used for the buttons
+    BUTTON_WIDTH = 250 # X size of the buttons
+    BUTTON_HEIGHT = 40 # Y size of the buttons
+    BUTTON_PADX = 15 # Padding X of the buttons
+    BUTTON_PADY = 7.5 # Padding Y of the buttons
+
+class Text:
+    """
+    Text in the different language
+    """
+
+    # English version
+    english = {
+        "Save Parameter": "Save Parameter",
+        "Parameter Manager": "Parameter Manager",
+        "Script Editor": "Script Editor",
+        "Update Card Material": "Update Card Material",
+    }
+
+    # Language selection
+    LANGUAGES = {
+        "eng": english,
+    }
--- src/window_build/main_window.py
+++ src/window_build/main_window.py
@@ -19,7 +19,7 @@ File Description:
 
 """ Import """
 # Import that can't be in the try
-from src.const import Return, Error, Window
+from src.const import Return, Error, Window, Text
 from sys import exit
 
 # Import that can be checked
@@ -55,6 +55,46 @@ def setup_geometry(window):
     # Setup the geometry of the window
     window.geometry(f"{Window.WIDTH}x{Window.HEIGHT}+{x}+{y}")
 
+def setup_tab(window):
+    """
+    Build the tab with the buttons: Save Parameter, Parameter Manager, Script Editor and Update Card Material
+
+    :param window: Main window of the app
+    """
+
+    # Tab that will contain the button
+    tab = ctk.CTkFrame(window, fg_color=Window.FRAME_COLOR, corner_radius=10)
+
+    # Build the different button
+    button_names = ["Save Parameter", "Parameter Manager", "Script Editor", "Update Card Material"]
+    button_functions = [print("Nop"), print("Nop"), print("Nop"), print("Nop")]
+    for i in range(len(button_names)):
+        button = ctk.CTkButton(tab, fg_color=Window.BUTTON_COLOR, text=Text.LANGUAGES["eng"][button_names[i]],
+            width=Window.BUTTON_WIDTH, height=Window.BUTTON_HEIGHT, corner_radius=10,
+            font=Window.BUTTON_FONT, command=lambda: button_functions[i])
+        button.grid(column=i, row=0, padx=Window.BUTTON_PADX, pady=Window.BUTTON_PADY)
+
+    # Setup the place of the tab
+    tab.place(x=(Window.WIDTH - (Window.BUTTON_PADX * len(button_names) * 2) - (Window.BUTTON_WIDTH * 4)) // 2, y=Window.FRAME_PADY)
+
+def setup_peripheral_device_list(window):
+    """
+    Build the list used for the peripheral device information display
+
+    :param window: Main window of the app
+    """
+
+    pass
+
+def setup_card_tab(window):
+    """
+    Build the tab with the buttons to setup / update the card connection
+
+    :param window: Main window of the app
+    """
+
+    pass
+
 def build(window):
     """
     Build the main window items (dispatch to sub functions)
@@ -68,5 +108,10 @@ def build(window):
     window.iconbitmap(default="data\\img\\MAGIC_icon_v3.ico")
     window.resizable(width=False, height=False) # To change in the future
     setup_geometry(window)
+
+    # Setup the interface
+    setup_tab(window)
+    setup_peripheral_device_list(window)
+    setup_card_tab(window)
 
     return Return.OK
